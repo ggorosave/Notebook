240fee7021b790e75e3e0986568eac9ef415b34e
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-11-27 15:48:41 -0700

    Added route for submitting notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,5 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 // GET route for retrieving all data
@@ -11,3 +12,31 @@ notes.get('/', (res, req) => {
 });
 
 // Route for submitting notes
+notes.post('/', (res, req) => {
+
+    // destructering the items in req.body
+    const { title, text } = req.body
+
+    // Checks for the required properties
+    if (title && text) {
+        const newNote = {
+            title,
+            test,
+            id: uuidv4(),
+        }
+
+        fs.appendFile('./db/db.json', newNote, (error) =>
+        error ? console.error(error) : console.log('New note added to database!'))
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note')
+    }
+});
+
+module.exports = notes;
