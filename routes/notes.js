de29e9fd0aaccbc91ec90b86d7590a660c41e2a1
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-11-28 18:00:35 -0700

    Added comments to server and routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,11 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const app = require('.');
-const { json } = require('express');
-
 
 // GET route for retrieving all data
 notes.get('/', (req, res) => {
 
+    // reads json file and returns parsed data to client
     fs.readFile('./db/db.json', 'utf8', (error, data) => {
         error ? console.error(error) : res.json(JSON.parse(data))
     });
@@ -20,15 +18,17 @@ notes.post('/', (req, res) => {
     // destructering the items in req.body
     const { title, text } = req.body;
 
-    // Checks for the required properties
+    // Checks for the required properties, title and text
     if (title && text) {
+
+        // Creates a new object for the note and uses uusid to assign it a unique id
         const newNote = {
             title,
             text,
             id: uuidv4(),
         }
 
-
+        // Calls the fs method to read the file, parses the data from the json file, pushes the new note to the existing array pulled from the data, the calls the fs method to rewrite the file containing the new note 
         fs.readFile('./db/db.json', 'utf8', (error, data) => {
             if (error) {
                 console.error(error);
@@ -36,35 +36,48 @@ notes.post('/', (req, res) => {
                 const parsedData = JSON.parse(data);
                 parsedData.push(newNote);
                 
+                // The '4' in the stringify method indicates that there should be 4 tabs after every property
                 fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (error) => error ? console.log(error) : console.log('New note added'))
 
             }
         });
 
+        // builds a response object to sent to the client
         const response = {
             status: 'success',
             body: newNote,
         };
 
+        // sends response to the client
         res.json(response);
     } else {
+
+        // sends an error to the client if required properties don't exist
         res.json('Error in posting note')
     }
 });
 
 // Route for deleting notes
 notes.delete('/:id', (req, res) => {
+
+    // variable that has the value of the parameter id
     const noteId = req.params.id;
     
+
     fs.readFile('./db/db.json', 'utf8', (error, data) => {
         if (error) {
             console.error(error);
         } else {
+            // parses the data from the json file
            const parsedData = JSON.parse(data);
+
+            // creates a new array that filters out the note with the given id from the parameter
            const result = parsedData.filter((note) => note.id !== noteId);
   
+            // rewrites the json file without the note with the matching id
            fs.writeFile('./db/db.json', JSON.stringify(result, null, 4), (error) => error ? console.log(error) : console.log(`Note deleted`));
-  
+            
+            // sends response to client
            res.json(`Note ${noteId} has been deleted`)
         }
   
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
+// imports routes
 const api = require('./routes/index.js')
-
-// Middleware?
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+// Lets us reference files with their relative paths instead of making a route for each individually
 app.use(express.static('public'));
 
 // GET Route for homepage
